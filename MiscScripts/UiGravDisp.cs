0e07ce8c6e09dd230e25e6586164ea6e84f85985
Tim Shmidt <timsh007@gmail.com>
2025-03-11 12:27:27 -0700

    small comment

--- MiscScripts/UiGravDisp.cs
+++ MiscScripts/UiGravDisp.cs
@@ -7,6 +7,6 @@ public class UiGravDisp : MonoBehaviour {
     void Update() {
         transform.localScale = new Vector3(Camera.main.orthographicSize * scale, Camera.main.orthographicSize * scale, 1); //scales the "arrow" such that it takes up the "scale" (i.e. 1/10) of the camera
         GetComponent<RectTransform>().localPosition = new Vector3(0, -1, 0); //keeps ui "arrow" 1 below the centerof the camera
-        GetComponent<RectTransform>().eulerAngles = new Vector3(0, 0, 0);
+        GetComponent<RectTransform>().eulerAngles = new Vector3(0, 0, 0); //makes it face down always
     }
 }
