923e964aaa524f4bf09491bff74f67588b5a16ff
Tim Shmidt <timsh007@gmail.com>
2025-03-11 11:27:32 -0700

    added comments

--- MiscScripts/UiGravDisp.cs
+++ MiscScripts/UiGravDisp.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class UiGravDisp : MonoBehaviour {
     public float scale;
     void Update() {
-        transform.localScale = new Vector3(Camera.main.orthographicSize * scale, Camera.main.orthographicSize * scale, 1);
-        GetComponent<RectTransform>().localPosition = new Vector3(0, -1, 0);
+        transform.localScale = new Vector3(Camera.main.orthographicSize * scale, Camera.main.orthographicSize * scale, 1); //scales the "arrow" such that it takes up the "scale" (i.e. 1/10) of the camera
+        GetComponent<RectTransform>().localPosition = new Vector3(0, -1, 0); //keeps ui "arrow" 1 below the centerof the camera
     }
 }
--- Scripts/CameraZoom.cs
+++ Scripts/CameraZoom.cs
@@ -7,11 +7,11 @@ public class CameraZoom : MonoBehaviour {
     [SerializeField] private int max = 100;
     void Update() {
         Camera camera = gameObject.GetComponent<Camera>();
-        camera.orthographicSize -= Input.mouseScrollDelta.y;
-        if (camera.orthographicSize > max) {
+        camera.orthographicSize -= Input.mouseScrollDelta.y; //adds mouse scroll to cam size
+        if (camera.orthographicSize > max) { //makes cam size unable to go above max
             camera.orthographicSize = max;
         }
-        if (camera.orthographicSize < min) {
+        if (camera.orthographicSize < min) { //makes cam size unable to go below min
             camera.orthographicSize = min;
         }
     }
