c1cafd34cecfeb51afcea8436feb15458d7008cd
Tim Shmidt <timsh007@gmail.com>
2025-03-11 13:30:32 -0700

    added different camerazoom and ui disp features for different types of cameras (othro and persp)

--- Scripts/CameraZoom.cs
+++ Scripts/CameraZoom.cs
@@ -3,16 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraZoom : MonoBehaviour {
-    private int min = 1;
-    [SerializeField] private int max = 100;
+    private int minO = 1; //min for ortho
+    [SerializeField] private int maxO = 100; //max for ortho
+    [SerializeField] private int minP = 10; //min for perspective
+    [SerializeField] private int maxP = 90; //max for perspective
     void Update() {
         Camera camera = gameObject.GetComponent<Camera>();
-        camera.orthographicSize -= Input.mouseScrollDelta.y; //adds mouse scroll to cam size
-        if (camera.orthographicSize > max) { //makes cam size unable to go above max
-            camera.orthographicSize = max;
-        }
-        if (camera.orthographicSize < min) { //makes cam size unable to go below min
-            camera.orthographicSize = min;
+        if (camera.orthographic) {
+            camera.orthographicSize -= Input.mouseScrollDelta.y; //adds mouse scroll to cam size
+            if (camera.orthographicSize > maxO) { //makes cam size unable to go above max
+                camera.orthographicSize = maxO;
+            }
+            if (camera.orthographicSize < minO) { //makes cam size unable to go below min
+                camera.orthographicSize = minO;
+            }
+        } else {
+            camera.fieldOfView -= 2 * Mathf.Atan(Input.mouseScrollDelta.y); //adds mouse scroll to cam fov, tangent for smooth cam movement
+            if (camera.fieldOfView > maxP) { //makes cam size unable to go above max
+                camera.fieldOfView = maxP;
+            }
+            if (camera.fieldOfView < minP) { //makes cam size unable to go below min
+                camera.fieldOfView = minP;
+            }
         }
     }
 }
