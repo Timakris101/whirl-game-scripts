8f0965dcc47e664d2a381884ec2353b41d03c46a
Tim Shmidt <timsh007@gmail.com>
2025-03-12 21:58:34 -0700

    improved bullet script and added a basic health system

--- Scripts/BulletScript.cs
+++ Scripts/BulletScript.cs
@@ -6,10 +6,23 @@ public class BulletScript : MonoBehaviour {
 
     private string bulletType;
     private float bulletInitSpeed = 10;
+    private float lifetime = 10;
+    private float timer = 0;
+    private float damage = 1;
 
-    
+    void OnCollisionEnter2D(Collision2D col) {
+        if (col.gameObject.GetComponent<Health>() != null) {
+            col.gameObject.GetComponent<Health>().removeHealth(damage);
+            Destroy(gameObject);
+        }
+    }
 
     void Start() {
         GetComponent<Rigidbody2D>().velocity = (Vector2) transform.right * bulletInitSpeed;
+    }
+
+    void Update() {
+        timer += Time.deltaTime;
+        if (timer > lifetime) Destroy(gameObject);
     }
 }
---
+++ Scripts/Health.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+    [SerializeField] private float maxHealth;
+    [SerializeField] private float health;
+    void Start() {
+        health = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (health <= 0) {
+            if (transform.tag != "Player") {
+                Destroy(gameObject);
+            } else {
+                //TODO:
+            }
+        }
+    }
+
+    public void removeHealth(float amt) {
+        health -= amt;
+    }
+}
