cad64d15a200c246f5f048edfc14d1de77c949ab
Tim Shmidt <timsh007@gmail.com>
2025-03-13 13:29:03 -0700

    made that actually the case because the rb vel is always zero and that is not right

--- Scripts/TurretEnemyLogic.cs
+++ Scripts/TurretEnemyLogic.cs
@@ -7,17 +7,21 @@ public class TurretEnemyLogic : MonoBehaviour {
     [SerializeField] private GameObject bullet;
     [SerializeField] private float timeBetweenShots;
     private float timer = 0;
+    private Vector2 nonRBVel;
+    private Vector2 prevPos;
 
     void Update() {
+        nonRBVel = ((Vector2) transform.position - prevPos) / Time.deltaTime;
         timer += Time.deltaTime;
         if (timer > timeBetweenShots) { //if timer do bullet things
             timer = 0; //reset timer
             spawnBullet();
         }
+        prevPos = (Vector2) transform.position;
     }
 
     void spawnBullet() {
         GameObject newBullet = Instantiate(bullet, transform.position + transform.right * transform.localScale.x, transform.rotation); //makes a new bullet at the gun launcher area
-        newBullet.GetComponent<Rigidbody2D>().velocity = (Vector2) transform.right * transform.localScale.x * newBullet.GetComponent<BulletScript>().getBulletInitSpeed() + GetComponent<Rigidbody2D>().velocity; //adds proper speed to the bullet
+        newBullet.GetComponent<Rigidbody2D>().velocity = (Vector2) transform.right * transform.localScale.x * newBullet.GetComponent<BulletScript>().getBulletInitSpeed() + nonRBVel; //adds proper speed to the bullet
     }
 }
