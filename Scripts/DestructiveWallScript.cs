20b17c15324f604acc8917d5ab325809fdab4da3
Tim Shmidt <timsh007@gmail.com>
2025-03-17 20:04:11 -0700

    made a destructive wall that destroys objects

---
+++ Scripts/DestructiveWallScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructiveWallScript : MonoBehaviour {
+    void OnCollisionStay2D(Collision2D col) {
+        if (col.transform.tag != "Surface") {
+            if (col.gameObject.GetComponent<Health>() != null) {
+                col.gameObject.GetComponent<Health>().removeHealth(1);
+            }
+        }
+    }
+}
