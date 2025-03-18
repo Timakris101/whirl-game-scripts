9ab1e26e0de18de427d0145ec4a2eab871a7939b
Tim Shmidt <timsh007@gmail.com>
2025-03-17 22:32:41 -0700

    made destructive wall do damage on enter as well

--- Scripts/DestructiveWallScript.cs
+++ Scripts/DestructiveWallScript.cs
@@ -3,6 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DestructiveWallScript : MonoBehaviour {
+    void OnCollisionEnter2D(Collision2D col) {
+        if (col.transform.tag != "Surface") {
+            if (col.gameObject.GetComponent<Health>() != null) {
+                col.gameObject.GetComponent<Health>().removeHealth(1);
+            }
+        }
+    }
+
     void OnCollisionStay2D(Collision2D col) {
         if (col.transform.tag != "Surface") {
             if (col.gameObject.GetComponent<Health>() != null) {
