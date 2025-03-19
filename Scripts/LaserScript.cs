0f5bf76c7ed8bbf5b2564d3c0adfdc4f26606679
Tim Shmidt <timsh007@gmail.com>
2025-03-19 13:57:31 -0700

    quick fix for raycast

--- Scripts/LaserScript.cs
+++ Scripts/LaserScript.cs
@@ -14,11 +14,13 @@ public class LaserScript : MonoBehaviour {
         RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.right); //cast a ray to the right
         lr.SetPosition(0, transform.position); //sets lr pos
         lr.SetPosition(1, hit ? hit.point : transform.position + transform.right * noHitMaxDist);
-        if (hit.transform.gameObject.GetComponent<RecieverScript>() != null) { //makes reciever active
-            hit.transform.gameObject.GetComponent<RecieverScript>().setActive();
-        }
-        if (hit.transform.gameObject.GetComponent<Health>() != null) { //does damage
-            hit.transform.gameObject.GetComponent<Health>().removeHealth(damage);
+        if (hit) {
+            if (hit.transform.gameObject.GetComponent<RecieverScript>() != null) { //makes reciever active
+                hit.transform.gameObject.GetComponent<RecieverScript>().setActive();
+            }
+            if (hit.transform.gameObject.GetComponent<Health>() != null) { //does damage
+                hit.transform.gameObject.GetComponent<Health>().removeHealth(damage);
+            }
         }
     }
 }
