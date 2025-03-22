909755948cc4bab8169e384dd6bea009581857be
Tim Shmidt <timsh007@gmail.com>
2025-03-21 17:34:25 -0700

    method decomposition

--- Scripts/GravBridgeScript.cs
+++ Scripts/GravBridgeScript.cs
@@ -6,6 +6,7 @@ public class GravBridgeScript : MonoBehaviour {
     private Vector3 startAngles;
     [SerializeField] private GameObject wall;
     [SerializeField] private float wallThickness = .2f;
+    [SerializeField] private float maxDist = 1000f;
 
     void Start() {
         startAngles = transform.eulerAngles;
@@ -14,13 +15,23 @@ public class GravBridgeScript : MonoBehaviour {
 
     void Update() {
         transform.eulerAngles = startAngles;
-        RaycastHit2D hitUp = Physics2D.Raycast(transform.position, transform.up);
-        Vector3 upEndPoint = hitUp.point; //"up" end point of wall
-        RaycastHit2D hitDown = Physics2D.Raycast(transform.position, -transform.up);
-        Vector3 downEndPoint = hitDown.point; //"down" end point of wall
+
+        Vector3 upEndPoint = positionOfHitInDir(1);
+        Vector3 downEndPoint = positionOfHitInDir(-1);
+
         Vector3 wallPos = (upEndPoint + downEndPoint) / 2; //avg between the two
         float wallLength = Vector3.Distance(upEndPoint, downEndPoint); //distance between up and down points
         wall.transform.position = wallPos;
         wall.transform.localScale = new Vector3(wallThickness, wallLength, 1);
+    }
+
+    private Vector3 positionOfHitInDir(int dir) {
+        RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, dir * transform.up); //checks all raycasts in a dir
+        foreach(RaycastHit2D hit in hits) {
+            if (hit.transform.tag == "Surface") { //if its not a surface dont stop the bridge
+                return hit.point;
+            }
+        }
+        return transform.position + transform.up * maxDist * dir; //if it hits nothing it goes veeerrryyy far
     }
 }
