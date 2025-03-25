1d906f16d4ae1e0a0b67dc7ffef13e72e3ef859d
Tim Shmidt <timsh007@gmail.com>
2025-03-24 20:43:22 -0700

    made a script to end a level

---
+++ Scripts/EndLevelScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndLevelScript : MonoBehaviour {
+    private GameObject electricityCircle;
+    private GameObject wireOutwards;
+    private GameObject endWire;
+    [SerializeField] private GameObject player;
+    private bool endLevel = false;
+
+    void OnTriggerEnter2D(Collider2D col) {
+        if (col.transform.tag == "Player") {
+            endLevel = true;
+            player = col.transform.gameObject;
+            col.transform.gameObject.GetComponent<PlayerController>().enabled = false;
+        }
+    }
+
+    void Start() {
+        endWire = transform.Find("EndWire").gameObject;
+        wireOutwards = transform.Find("WireOutwards").gameObject;
+        electricityCircle = transform.Find("ElectricityCircle").gameObject;
+    }
+
+    void Update() {
+        if (endLevel) {
+            player.transform.position += (transform.position - player.transform.position) * Time.deltaTime;
+            electricityCircle.transform.position += (endWire.transform.position - transform.position) * Time.deltaTime;
+            if (!electricityCircle.GetComponent<ParticleSystem>().isPlaying) electricityCircle.GetComponent<ParticleSystem>().Play();
+            if (Vector3.Distance(electricityCircle.transform.position, endWire.transform.position) < .1f) SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
