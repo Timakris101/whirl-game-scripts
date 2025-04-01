95a762d3d62ecc9e858d7d68e489ff3e30259aad
Tim Shmidt <timsh007@gmail.com>
2025-04-01 11:01:49 -0700

    small change to the "animation" for end lvl

--- Scripts/EndLevelScript.cs
+++ Scripts/EndLevelScript.cs
@@ -26,12 +26,15 @@ public class EndLevelScript : MonoBehaviour {
 
     void Update() {
         if (endLevel) {
-            player.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
-            player.transform.position += (transform.position - player.transform.position) * Time.deltaTime;
-            player.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, 1f);
-            if (!(Vector3.Distance(electricityCircle.transform.position, endWire.transform.position) < .1f)) electricityCircle.transform.position += (endWire.transform.position - transform.position) * Time.deltaTime;
-            if (!electricityCircle.GetComponent<ParticleSystem>().isPlaying) electricityCircle.GetComponent<ParticleSystem>().Play();
-            if (Vector3.Distance(electricityCircle.transform.position, endWire.transform.position) < .1f) SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            if (Vector3.Distance(player.transform.position, transform.position) < 1.25f) {
+                if (!(Vector3.Distance(electricityCircle.transform.position, endWire.transform.position) < .1f)) electricityCircle.transform.position += (endWire.transform.position - transform.position) * Time.deltaTime;
+                if (!electricityCircle.GetComponent<ParticleSystem>().isPlaying) electricityCircle.GetComponent<ParticleSystem>().Play();
+                if (Vector3.Distance(electricityCircle.transform.position, endWire.transform.position) < .1f) SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            } else {
+                player.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+                player.transform.position += (transform.position - player.transform.position) * Time.deltaTime;
+                player.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, 1f);
+            }
         }
     }
 }
