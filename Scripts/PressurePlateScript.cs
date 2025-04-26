9bfe3e1f670bbe268a8702561e2b59a38288d828
Tim Shmidt <timsh007@gmail.com>
2025-04-25 22:58:42 -0700

    improved pressure plate logic

--- Scripts/PressurePlateScript.cs
+++ Scripts/PressurePlateScript.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class PressurePlateScript : MonoBehaviour {
     [SerializeField] private bool on;
+    [SerializeField] private List<GameObject> contacts;
 
     void OnCollisionEnter2D(Collision2D col) {
         on = true;
+        contacts.Add(col.gameObject);
     }
 
     void OnCollisionStay2D(Collision2D col) {
@@ -14,7 +16,8 @@ public class PressurePlateScript : MonoBehaviour {
     }
 
     void OnCollisionExit2D(Collision2D col) {
-        on = false;
+        contacts.Remove(col.gameObject);
+        if (contacts.Count == 0) on = false;
     }
 
     void Update() {
