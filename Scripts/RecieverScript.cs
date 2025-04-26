f598153150873d7c256a8f45dae1a1909072ddb9
Tim Shmidt <timsh007@gmail.com>
2025-04-26 13:00:40 -0700

    modified recieverscript to make more sense

--- Scripts/RecieverScript.cs
+++ Scripts/RecieverScript.cs
@@ -5,22 +5,10 @@ using UnityEngine;
 public class RecieverScript : MonoBehaviour {
     [SerializeField] private GameObject doorConnected;
     [SerializeField] private List<GameObject> wiresConnected;
-    [SerializeField] private int timeActive;
-    [SerializeField] private int time;
-
-    void Update() {
-        time++;
-        if (time > timeActive + 1) { //if it has been active for less time than the time then it must be deactivated
-            setInactive();
-            time = 0;
-            timeActive = 0;
-        }
-    }
 
     public void setActive() {
         doorConnected.GetComponent<DoorScript>().open();
         foreach (GameObject wireConnected in wiresConnected) wireConnected.GetComponent<WireScript>().light();
-        timeActive++;
     }
 
     public void setInactive() {
