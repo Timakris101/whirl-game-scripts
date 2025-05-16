30d3378506017f87c9f409f542226013be43d3ec
Tim Shmidt <timsh007@gmail.com>
2025-05-15 18:18:50 -0700

    completed dialogue system

--- Scripts/DialogueManager.cs
+++ Scripts/DialogueManager.cs
@@ -8,19 +8,70 @@ public class DialogueManager : MonoBehaviour {
     [SerializeField] private Dialogue[] dialogues;
     [SerializeField] private int indexConversation = -1;
     [SerializeField] private int indexSentence = -1;
+    [SerializeField] private float indexLetter = -1;
+    [SerializeField] private float writingSpeed;
+    [SerializeField] private bool conversationOn;
+
+    [SerializeField] private GameObject dialogueTextBox;
+    [SerializeField] private GameObject nameTextBox;
+    [SerializeField] private GameObject continueButton;
+    [SerializeField] private GameObject spriteArea;
 
     public void startConversation() {
         indexConversation = 0;
         indexSentence = 0;
+        indexLetter = 0;
+        conversationOn = true;
+    }
+
+    public void endConversation() {
+        dialogueTextBox.GetComponent<TextMeshProUGUI>().text = "";
+        nameTextBox.GetComponent<TextMeshProUGUI>().text = "";
+        spriteArea.SetActive(false);
+        continueButton.SetActive(false);
+        conversationOn = false;
     }
 
     public void goToNextDialogue() {
         indexConversation++;
         indexSentence = 0;
+        indexLetter = 0;
+
+        if (indexConversation >= dialogues.Length) {
+            endConversation();
+        }
     }
 
-    public void dispSentence(int indexConversation, int indexSentence) {
-        Debug.Log(dialogues[indexConversation].getSentence(indexSentence));
+    public void goToNextSentence() {
+        indexSentence++;
+        indexLetter = 0;
+
+        if (indexSentence >= dialogues[indexConversation].getSentences().Length) {
+            goToNextDialogue();
+        }
+    }
+
+    public void clickedContinue() {
+        if (conversationOn) {
+            if (sentenceComplete()) {
+                goToNextSentence();
+            } else {
+                indexLetter = dialogues[indexConversation].getSentence(indexSentence).Length;
+            }
+        }
+    }
+
+    public void dispDialogue() {
+        if (!sentenceComplete()) {
+            indexLetter += Time.deltaTime * writingSpeed;
+        }
+        nameTextBox.GetComponent<TextMeshProUGUI>().text = dialogues[indexConversation].getName() + ": ";
+        dialogueTextBox.GetComponent<TextMeshProUGUI>().text = dialogues[indexConversation].getSentence(indexSentence).Substring(0, (int) indexLetter);
+        spriteArea.GetComponent<UnityEngine.UI.Image>().sprite = dialogues[indexConversation].getSprite();
+    }
+
+    private bool sentenceComplete() {
+        return !(indexLetter < dialogues[indexConversation].getSentence(indexSentence).Length);
     }
 
     void Start() {
@@ -28,6 +79,6 @@ public class DialogueManager : MonoBehaviour {
     }
 
     void Update() {
-        dispSentence(indexConversation, indexSentence);
+        if (conversationOn) dispDialogue();
     }
 }
