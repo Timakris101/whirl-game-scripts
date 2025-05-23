fb66329b8c040dc28cf07b46768062fa59e149ec
Tim Shmidt <timsh007@gmail.com>
2025-05-22 22:20:33 -0700

    moved saveload functions into main menu manager and added a quit function

--- Scripts/MainMenuManager.cs
+++ Scripts/MainMenuManager.cs
@@ -13,4 +13,17 @@ public class MainMenuManager : MonoBehaviour {
     void Update() {
         continueButton.SetActive(PlayerPrefs.GetInt("mostRecentSceneIndex") == 1);
     }
+
+    public void restart() {
+        PlayerPrefs.SetInt("mostRecentSceneIndex", 0);
+        load();
+    }
+
+    public void load() {
+        SceneManager.LoadScene(PlayerPrefs.GetInt("mostRecentSceneIndex"));
+    }
+
+    public void quit() {
+        Application.Quit();
+    }
 }
