a545521840dd45d9b0c43e40370abff6210dc900
Tim Shmidt <timsh007@gmail.com>
2025-05-25 21:09:58 -0700

    changed misslescript a bit

--- Scripts/MissileScript.cs
+++ Scripts/MissileScript.cs
@@ -19,7 +19,7 @@ public class MissileScript : MonoBehaviour {
     }
 
     void Start() {
-        GetComponent<Rigidbody2D>().velocity = new Vector2(0, speed);
+        GetComponent<Rigidbody2D>().velocity = speed * transform.up;
     }
 
     void Update() {
